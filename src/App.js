1a8d137f721195a9079d2f95c8ee3992e6e9c3f5
MCkeydev <mattcourrejou@gmail.com>
2021-11-25 10:31:31 +0100

    implemented navbar header in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Routes, Route, useParams, useLocation } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop-page/shop.component";
+import Header from "./components/header/header.componenet";
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
